b27b8d8821d9bf5fc2b7acfbfe87a4759f19345c
wilsonfong82 <wilsonfong82@gmail.com>
2019-11-17 19:33:13 -0500

    update routes

--- app.js
+++ app.js
@@ -1,89 +1,59 @@
-const express = require ('express');
-const session = require('express-session');
-const routes = require('./server/routes');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const path = require('path');
-const fileUpload = require('express-fileupload');
-const defaultErrorHandler = require('./server/middleware/errorHandler');
-const authenticate = require('./server/middleware/authentication');
-require('dotenv').config();
+const express = require("express");
+const session = require("express-session");
+const routes = require("./server/routes");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const defaultErrorHandler = require("./server/middleware/errorHandler");
+const authenticate = require("./server/middleware/authentication");
+require("dotenv").config();
 
 const app = express();
-
 
 const port = 3000;
 
 const db = mysql.createConnection({
   // Replace with user-appropriate values
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'tutors_db',
+  host: "localhost",
+  user: "root",
+  password: "uGotY0rked",
+  database: "tutors_db",
   insecureAuth: true
 });
 
-db.connect((err) => {
-  if(!err) {
+db.connect(err => {
+  if (!err) {
     console.log("Connected");
-  }
-  else {
-    throw(err)
+  } else {
+    throw err;
   }
 });
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure:false,
-        maxAge: 7200000,
-        httpOnly: true,
-    },
-}));
-
+      secure: false,
+      maxAge: 7200000,
+      httpOnly: true
+    }
+  })
+);
 
 app.set("port", process.env.port || port);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
 app.use(authenticate.parseUser);
 app.use(defaultErrorHandler);
 app.use(routes);
 
-app.get("/", (req, res) => {
-    res.render("landing");
-  });
-  
-  app.get("/register", (req, res) => {
-    res.render("register");
-  });
-  
-  app.get("/login", (req, res) => {
-    res.render("login");
-  });
-  
-  app.get("/register", (req, res) => {
-    res.render("register");
-  });
-  
-  app.get("/results", (req, res) => {
-    res.render("results");
-  });
-  
-  app.get("/search", (req, res) => {
-    res.render("search");
-  });
-  
-  app.get("/home", (req, res) => {
-    res.render("home");
-  });
-  
-  app.listen(port, () => {
-      console.log(`Server running on port: ${port}`);
-  });
-  
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,31 @@
-const routes = require('express').Router();
+const routes = require("express").Router();
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+routes.get("/", (req, res) => {
+  res.render("landing");
 });
 
+routes.get("/register", (req, res) => {
+  res.render("register");
+});
+
+routes.get("/login", (req, res) => {
+  res.render("login");
+});
+
+routes.get("/register", (req, res) => {
+  res.render("register");
+});
+
+routes.get("/results", (req, res) => {
+  res.render("results");
+});
+
+routes.get("/search", (req, res) => {
+  res.render("search");
+});
+
+routes.get("/home", (req, res) => {
+  res.render("home");
+});
 module.exports = routes;
 // comment
