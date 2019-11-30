3c203050ee33e5afdeba29befccbfd7f27227cb5
manpreet saini <manpreet_saini92@hotmail.com>
2019-11-30 16:51:43 -0500

    new dependencies added

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,8 +4,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const passport = require('passport');
+
+const bcrypt = require('bcrpytjs');
+
 router.get('/login', (req, res) => {
   res.render('login')
 });
- 
+
 module.exports = router;
